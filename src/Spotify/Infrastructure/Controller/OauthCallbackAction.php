ee498e175c261dccda89944b714d3ce5a21edaaf
Konrad Szelepusta <konrad.szelepusta@gmail.com>
2024-05-07 20:38:37 +0200

    update

--- src/Spotify/Infrastructure/Controller/OauthCallbackAction.php
+++ src/Spotify/Infrastructure/Controller/OauthCallbackAction.php
@@ -35,7 +35,7 @@ final readonly class OauthCallbackAction
 
         $redirectUrl = sprintf(
             '%s/session/%s/confirmation',
-            'http://localhost:3000',
+            'http://api.grabniolec.pl',
             $state
         );
 
